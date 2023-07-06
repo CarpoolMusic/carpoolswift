b93310a6fce22fadef94ab964c1a01bacc0e2411
poles <biscao@seas.upenn.edu>
2023-07-06 16:25:31 -0400

    passing basic API tests

--- backend/myApp/tests/users.test.js
+++ backend/myApp/tests/users.test.js
@@ -48,3 +48,64 @@ describe('POST /users', () => {
         }
     })
 });
+
+describe('UPDATE users/:id', () => {
+    let createdUserId;
+  
+    // Before any tests run, create a user
+    beforeAll(async () => {
+      const res = await request(app)
+        .post('/users')
+        .send({
+          name: 'Test User',
+          email: 'test@test.com',
+          spotify_id: '123456'
+        })
+  
+      createdUserId = res.body.id;
+    })
+  
+    // After all tests have run, delete the user
+    afterAll(async () => {
+      if (createdUserId) {
+        await request(app)
+          .delete(`/users/${createdUserId}`)
+      }
+    })
+  
+    it('should update an existing user', async () => {
+      const res = await request(app)
+        .put(`/users/${createdUserId}`)
+        .send({
+          name: 'Updated Name',
+          email: 'updated@test.com'
+        })
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toHaveProperty('id')
+      expect(res.body.name).toBe('Updated Name')
+      expect(res.body.email).toBe('updated@test.com')
+    })
+  });
+
+  describe('DELETE existing user', () => {
+    let createdUserId;
+  
+    // Before any tests run, create a user
+    beforeEach(async () => {
+      const res = await request(app)
+        .post('/users')
+        .send({
+          name: 'Test User',
+          email: 'test@test.com',
+          spotify_id: '123456'
+        })
+      createdUserId = res.body.id;
+    })
+  
+    it('should delete an existing user', async () => {
+      const res = await request(app)
+        .delete(`/users/${createdUserId}`)
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toEqual({ message: "User was deleted successfully." })
+    })
+  });
