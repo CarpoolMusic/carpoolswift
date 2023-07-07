e3de6cda51f6982dce7d4cd8ceb3e9179efdf533
poles <biscao@seas.upenn.edu>
2023-07-07 14:12:07 -0400

    more socket methods

--- backend/myApp/app.js
+++ backend/myApp/app.js
@@ -77,6 +77,55 @@ io.on('connection', (socket) => {
     }
   });
 
+  // When a user wants to leave a session (non-admin)
+  socket.on('leave session', (sessionID) => {
+    const session = sessionManager.getSession(sessionID);
+
+    if (session) {
+      if (session.members.has(socket.id)) {
+        socket.leave(sessionID);
+        SessionManager.removeMemberFromSession(sessionID, socket.id); // Remove member from session
+
+        socket.emit('left session', sessionID); // Notify the user they've left the session
+        io.in(sessionID).emit('member left', socket.id); // Notify others in the room that a member has left
+      } else {
+        socket.emit('error', 'You are not a member of this session');
+      }
+    } else {
+      socket.emit('error', 'Invalid session ID');
+    }
+  });
+
+  // When a user wants to delete a session
+  socket.on('delete session', ({ userID, sessionID }) => {
+    const session = SessionManager.getSession(sessionID);
+
+    if (session) {
+      // Only allow the session host (admin) to delete the session
+      if (session.host === userID) {
+
+        // Disconnect all sockets associated with this session
+        io.of('/').in(sessionID).clients((error, socketIds) => {
+          if (error) throw error;
+          socketIds.forEach(socketId => io.sockets.sockets[socketId].disconnect(true));
+        });
+
+        // Delete session from session manager
+        SessionManager.deleteSession(sessionID);
+        socket.leave(sessionID); // Leave the socket room
+
+        // Broadcast message to other users in the session
+        socket.to(sessionID).emit('session deleted', { message: 'The session has been deleted by the host.' });
+
+        socket.emit('session deleted', sessionID);
+      } else {
+        socket.emit('error', 'Only the host can delete the session.');
+      }
+    } else {
+      socket.emit('error', 'Invalid session ID');
+    }
+  });
+
   // Example: a custom event that sends a message to the client
   socket.on('custom event', data => {
     console.log(data);
@@ -85,8 +134,8 @@ io.on('connection', (socket) => {
   // Disconnect event
   socket.on('disconnect', () => {
     console.log('Client disconnected');
-  })
-})
+  });  
+});
 
 // error handler
 app.use(function(err, req, res, next) {
